286035c073612202d7920586415892726a06b667
Asad Sajjad <asad.sajjad.codes@gmail.com>
2026-08-20 04:32:16 +0400

    feat: add regex filename sanitization

--- app/cleaner.py
+++ app/cleaner.py
@@ -1,14 +1,30 @@
+import re
 from pathlib import Path
 
-# cleans the file name 
-def clean_filename(filename : str) ->str:
+def clean_filename(filename: str) -> str:
+    """
+    Clean a filename by removing special characters and replacing spaces with underscores and preserve
+    the extension of the filename."""
 
-    path = Path(filename).resolve()
+    path = Path(filename)
 
     stem = path.stem
-    ex = path.suffix
+    extension = path.suffix
 
     stem = stem.strip()
-    stem = stem.replace(" ", "_")
-    return stem + ex
+    stem = re.sub(r"[^a-zA-Z0-9]+", "_", stem) # remove special characters and spaces with underscores.
+    stem = stem.strip("_")
+    stem = stem.lower()
 
+    return stem + extension
+
+
+# test the function with some filenames .
+print(clean_filename("example file.txt"))
+print(clean_filename("   My!!! Vacation@@@ Photos ### 2026 (FINAL).jpg"))
+print(clean_filename("2023-07 data analysis.xlsx"))
+print(clean_filename("My Vacation Photo.jpg"))
+print(clean_filename("My   Vacation   Photo.jpg"))
+print(clean_filename("My @ Vacation # Photo!!.jpg"))
+print(clean_filename("   Summer---Beach!!!.png   "))
+print(clean_filename("Project###2026###Final.mp4"))
